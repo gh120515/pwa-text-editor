dad2801dcc32431e73ed3633a480c38417c92988
gh120515 <123872197+gh120515@users.noreply.github.com>
2023-07-30 13:04:34 +0930

    database logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,13 +12,45 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   
-  console.error('putDb not implemented');
-}
+  console.log('PUT to the database');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Create a connection to the database database and version we want to use.
+  const textDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges (read & write to update data)
+  const tx = textDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .put() method to get all data in the database.
+  const request = store.put({ content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+
+  // console.error('putDb not implemented');
+};
+
+// Logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const textDb = await openDB('jate', 1);
+  const tx = textDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+
+  // console.error('getDb not implemented');
+};
 
 initdb();
